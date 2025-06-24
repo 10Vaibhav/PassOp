a2ed432c5b63b51592f94f2f14b1624e025f05b9
Vaibhav Mahajan <143709642+10Vaibhav@users.noreply.github.com>
2025-06-24 08:02:04 +0530

    Saving Passwords to local storage

--- src/components/Manager.jsx
+++ src/components/Manager.jsx
@@ -1,17 +1,37 @@
-import { useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 
 const Manager = () => {
-
   const ref = useRef();
 
-  const showPassword = () => {
-    // alert("show the password");
-    if(ref.current.src.includes("icons/hidden.png")){
-      ref.current.src = "icons/eye.png"
-    }else{
-      ref.current.src = "icons/hidden.png"
+  const [form, setform] = useState({ site: "", username: "", password: "" });
+  const [passwordArray, setPasswordArray] = useState([]);
+
+  useEffect(() => {
+    let passwords = localStorage.getItem("passwords");
+
+    if (passwords) {
+      setPasswordArray(JSON.parse(passwords));
     }
-  }
+  }, []);
+
+  const showPassword = () => {
+    if (ref.current.src.includes("icons/hidden.png")) {
+      ref.current.src = "icons/eye.png";
+    } else {
+      ref.current.src = "icons/hidden.png";
+    }
+  };
+
+  const savePassword = () => {
+    console.log(form);
+    setPasswordArray([...passwordArray, form]);
+    localStorage.setItem("passwords", JSON.stringify([...passwordArray, form]))
+    console.log([...passwordArray, form]);
+  };
+
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
 
   return (
     <>
@@ -34,7 +54,9 @@ const Manager = () => {
             className="rounded-full border border-green-500 w-full px-4 py-1"
             placeholder="Enter Website URL"
             type="text"
-            name=""
+            value={form.site}
+            onChange={handleChange}
+            name="site"
             id=""
           />
           <div className="flex w-full justify-between gap-8">
@@ -42,7 +64,9 @@ const Manager = () => {
               className="rounded-full border border-green-500 w-full px-4 py-1"
               placeholder="Enter Username"
               type="text"
-              name=""
+              value={form.username}
+              onChange={handleChange}
+              name="username"
               id=""
             />
             <div className="relative">
@@ -50,15 +74,29 @@ const Manager = () => {
                 className="rounded-full border border-green-500 w-full px-4 py-1"
                 placeholder="Enter Password"
                 type="text"
-                name=""
+                value={form.password}
+                onChange={handleChange}
+                name="password"
                 id=""
               />
-              <span className="absolute right-[3px] top-[4px] cursor-pointer" onClick={showPassword}>
-                <img ref={ref} className="p-1" width={26} src="/icons/eye.png" alt="eye" />
+              <span
+                className="absolute right-[3px] top-[4px] cursor-pointer"
+                onClick={showPassword}
+              >
+                <img
+                  ref={ref}
+                  className="p-1"
+                  width={26}
+                  src="/icons/eye.png"
+                  alt="eye"
+                />
               </span>
             </div>
           </div>
-          <button className="flex gap-2 cursor-pointer justify-center items-center bg-green-400 hover:bg-green-300 hover:scale-99 rounded-full px-8 py-2 w-fit border border-green-900">
+          <button
+            onClick={savePassword}
+            className="flex gap-2 cursor-pointer justify-center items-center bg-green-400 hover:bg-green-300 hover:scale-99 rounded-full px-8 py-2 w-fit border border-green-900"
+          >
             <lord-icon
               src="https://cdn.lordicon.com/efxgwrkc.json"
               trigger="hover"
